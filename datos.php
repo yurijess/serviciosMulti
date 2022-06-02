5d2b530ab1c8b7a8c63ff49dd486122705a0dcc2
yuri bri <tareausb@gmail.com>
2022-06-02 08:57:24 -0500

    hay cambio

---
+++ datos.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+$a=$_GET['velocidad'];
+$b=$_GET['direccion'];
+//--------------------------------------
+$client = new MongoDB\Client(
+    'mongodb+srv://yuri:123@cluster0.kkviz.mongodb.net/pronostico?retryWrites=true&w=majority');
+
+$tb=$client->pronostico->araucaclima;
+$filter= ['$and'=>
+            [
+                ['VelocidaddelViento'=>['$eq'=>$a]],
+                ['DirecciondelViento'=>['$eq'=>$b]]
+            ]
+        ];
+
+$rows = $tb->find($filter);
+$datos= iterator_to_array($rows);
+echo json_encode($datos);
+/*$registro=array(
+"iduser"=>"1458",
+"fecha"=>"1/1/2033",
+"puntaje"=>"14589"
+);*/
+
+//$resultado=$tb->insertOne($registro);
+//echo $resultado->getInsertedCount();
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+$arr=array("envio" => "cambio",
+        "me"=>"serviciosmultimedia");
+echo json_encode($arr);
+?>
