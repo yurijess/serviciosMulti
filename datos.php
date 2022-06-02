ac57e0b7c7c0bed4dcc637baa671cbe5497422cf
yuri bri <tareausb@gmail.com>
2022-06-02 09:06:03 -0500

    f

--- datos.php
+++ datos.php
@@ -4,10 +4,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 $a=$_GET['velocidad'];
 $b=$_GET['direccion'];
 //--------------------------------------
-/*
+
 $client = new MongoDB\Client(
     'mongodb+srv://yuri:123@cluster0.kkviz.mongodb.net/pronostico?retryWrites=true&w=majority');
-
+/*
 $tb=$client->pronostico->araucaclima;
 $filter= ['$and'=>
             [
